a5bae9d1898085be97a586e0ebb0625d69c5e446
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-05-12 11:47:38 +0700

    rebuilt

---
+++ unlocker/celery.py
@@ -0,0 +1,12 @@
+# celery.py
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webFaceUnlock.settings')
+
+app = Celery('webFaceUnlock')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
